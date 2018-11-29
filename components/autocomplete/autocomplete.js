e4596d022f3869ef8fc858422a786ac0bc72628d
ElevaTI <git@elevati.com.br>
2018-11-29 19:55:44 -0200

    -

--- components/autocomplete/autocomplete.js
+++ components/autocomplete/autocomplete.js
@@ -559,17 +559,17 @@ var AutoComplete = /** @class */ (function () {
         this.focus = false;
         this.onModelTouched();
         this.onBlur.emit(event);
-        if (this.multiple && this.multiInputEL.nativeElement.value && !this.forceSelection) {
-            var newItem = {};
-            newItem['acao'] = 1;
-            if (this.podeAdicionar) {
-                newItem['isAdd'] = true;
-            }
-            newItem[this.colunaOpcao] = this.multiInputEL.nativeElement.value;
-            newItem[this.colunaChip] = this.multiInputEL.nativeElement.value;
-            newItem[this.field] = this.multiInputEL.nativeElement.value;
-            this.selectItem(newItem);
-        }
+        /*if (this.multiple && this.multiInputEL.nativeElement.value && !this.forceSelection) {
+          let newItem = {};
+          newItem['acao'] = 1;
+          if (this.podeAdicionar) {
+            newItem['isAdd'] = true;
+          }
+          newItem[this.colunaOpcao] = this.multiInputEL.nativeElement.value;
+          newItem[this.colunaChip] = this.multiInputEL.nativeElement.value;
+          newItem[this.field] = this.multiInputEL.nativeElement.value;
+          this.selectItem(newItem);
+        }*/
     };
     AutoComplete.prototype.onInputChange = function (event) {
         if (this.forceSelection && this.suggestions) {
