605da80fd98b2517cef27d25602873a648ef9f62
ThiagoKlug <36234756+ThiagoKlug@users.noreply.github.com>
2020-12-04 10:06:04 -0300

    update teste combo
    console

--- components/input-combo/input-combo.js
+++ components/input-combo/input-combo.js
@@ -81,6 +81,7 @@ var InputCombo = /** @class */ (function () {
     InputCombo.prototype.ngOnInit = function () {
         this.optionsToDisplay = this.options;
         this.updateSelectedOption(null);
+        console.log("teste combo");
     };
     Object.defineProperty(InputCombo.prototype, "options", {
         get: function () {
